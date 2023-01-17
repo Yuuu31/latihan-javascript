57486d496281d933af5bc62bf72b6b26620df9c4
Yuuu31 <wahyulalu311203@gmail.com>
2023-01-17 18:50:43 +0800

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,101 @@
-console.log("belajar 1")
+// console.log("Latihan Javascript")
+// let merk = "brio honda"
+// let jumlahRoda = ("4","6","8")
+// let jenisBahanBakar = "solar"
+// console.log("mobil ini bermerk:brio honda")
+// console.log("jumlah rodanya:4,6,8")
+// console.log("jenis bensin yang digunakan:solar")
+// let jumlahRumahBtn = "5 rumah"
+// console.log("ada sebanyak:5 rumah")
+
+// let nama = "wahyu"
+// var berat= "56 kg"
+// const gender= "perempuan"
+
+// nama="siti"
+// berat="60"
+// console.log("namaa:wahyu")
+// console.log("berat:56")
+// console.log("gender:Laki-laki")
+// console.log("gender:perempuan")
+
+// let barang="gelas"
+// barang="gelas"
+// console.log("barang ini berupa:gelas")
+// let buah=("apel")
+// buah=("mangga")
+// console.log("buah:apel,mangga")
+
+// let total=30*15
+// console.log("total:")
+// let modal=100
+// let hargaJual=150
+// let keuntungan=(hargaJual-modal) === 50
+// console.log("keuntungan:50")
+
+
+// let merk="smanda"
+// function aksesVariableLuar(){
+//     console.log("merk:smanda")
+// }
+// aksesVariableLuar("merk")
+// console.log("merk:smanda")
+
+
+// let merk="smanda"
+// merk="smansa"
+// function ubahVariableLuar(){
+//     merk="smansa"
+// }
+// console.log("merk:smansa")
+// ubahVariableLuar ("merk:smansa")
+
+// function cekMobilYangDigunakan(){
+//     let merkMobilYangDigunakan="Mazda"
+//     console.log("merkMobilYangDigunakan")
+// }
+// cekMobilYangDigunakan()
+// console.log("cekMobilYangDigunakan")
+
+// let transportasi="truk"
+
+// function cekBahanBakarAwal(){
+//     let JenisBahanBakar="solar"
+//     console.log("jenisBahanBakar")
+// }
+// function cekBahanBakarUlang(){
+//     let jenisBahanBakarUlang="dexlite"
+//     console.log("jenisBahanBakar")
+// }
+// cekBahanBakarUlang("dexlite")
+// cekBahanBakarAwal("solar")
+
+// let buah="mangga"
+// function definisiulang(){
+//     let buah="jeruk"
+//     console.log("buah:mangga")
+// }
+// definisiulang()
+// console.log("buah:jeruk")
+
+
+// let string1="selamat"
+// let string2='datang'
+// let gabunganBiasa=string1+"selamat"+string2+"datang";
+// console.log("selamat datang")
+// let gabunganES6=`${string1} ${string2}`
+// console.log("selamat datang")
+
+// let number1=30
+// let number2=07
+// let number3=20
+// let number4=40
+// let number5=100
+// let total=number4/number3+number1
+
+// let decimal=10.2
+// let decimalJuga=30.10
+
+
+let isTrue = true
+let isFalse = false
